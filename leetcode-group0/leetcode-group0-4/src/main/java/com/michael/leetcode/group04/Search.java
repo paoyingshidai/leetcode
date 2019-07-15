79f0a29ad3aa63bcfdfce4fb24e46f2993c25b8d
Michael <13432849094@163.com>
2019-07-15 17:40:24 +0800

    二分查找（升序数组反转查找）

--- leetcode-group0/leetcode-group0-4/src/main/java/com/michael/leetcode/group04/Search.java
+++ leetcode-group0/leetcode-group0-4/src/main/java/com/michael/leetcode/group04/Search.java
@@ -28,8 +28,7 @@ import java.util.Arrays;
  */
 public class Search {
 
-    public int search(int[] nums, int target) {
-
+    public int search3(int[] nums, int target) {
         Arrays.sort(nums);
         int lo = 0;
         int hi = nums.length;
@@ -47,11 +46,99 @@ public class Search {
         return -1;
     }
 
+
+
+
+    /**********************************************************************************/
+    int [] nums;
+    int target;
+
+    /**
+     * 找到最小的值（即旋转点）
+     * @param left
+     * @param right
+     * @return
+     */
+    public int find_rotate_index(int left, int right) {
+        if (nums[left] < nums[right])
+            return 0;
+
+        while (left <= right) {
+            int pivot = (left + right) / 2;
+            if (nums[pivot] > nums[pivot + 1])
+                return pivot + 1;
+            else {
+                if (nums[pivot] < nums[left])
+                    right = pivot - 1;
+                else
+                    left = pivot + 1;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * 旋转点
+     * @param left
+     * @param right
+     * @return
+     */
+    public int search(int left, int right) {
+    /*
+    Binary search
+    */
+        while (left <= right) {
+            int pivot = (left + right) / 2;
+            if (nums[pivot] == target)
+                return pivot;
+            else {
+                if (target < nums[pivot])
+                    right = pivot - 1;
+                else
+                    left = pivot + 1;
+            }
+        }
+        return -1;
+    }
+
+    public int search(int[] nums, int target) {
+        this.nums = nums;
+        this.target = target;
+
+        int n = nums.length;
+
+        if (n == 0)
+            return -1;
+        if (n == 1)
+            return this.nums[0] == target ? 0 : -1;
+
+        // 找到旋转点
+        int rotate_index = find_rotate_index(0, n - 1);
+
+        // if target is the smallest element
+        if (nums[rotate_index] == target)
+            return rotate_index;
+        // if array is not rotated, search in the entire array
+        if (rotate_index == 0)
+            return search(0, n - 1);
+        if (target < nums[0])
+            // search in the right side
+            return search(rotate_index, n - 1);
+        // search in the left side
+        return search(0, rotate_index);
+    }
+//
+//    作者：LeetCode
+//    链接：https://leetcode-cn.com/problems/two-sum/solution/sou-suo-xuan-zhuan-pai-xu-shu-zu-by-leetcode/
+//    来源：力扣（LeetCode）
+//    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
     public static void main(String[] args) {
 
         Search search = new Search();
         int[] data = new int[]{4, 5, 6, 7, 0, 1, 2};
-        System.out.println(search.search(data, 0));
+        System.out.println(search.search3(data, 0));
 
     }
 
