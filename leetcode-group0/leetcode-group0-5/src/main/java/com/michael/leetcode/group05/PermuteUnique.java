59eb03a93e3edd203cab506f959c8751146a4b52
Michael <13432849094@163.com>
2019-11-25 10:39:26 +0800

    commit

--- leetcode-group0/leetcode-group0-5/src/main/java/com/michael/leetcode/group05/PermuteUnique.java
+++ leetcode-group0/leetcode-group0-5/src/main/java/com/michael/leetcode/group05/PermuteUnique.java
@@ -27,6 +27,18 @@ public class PermuteUnique {
     private List<List<Integer>> res = new ArrayList<>();
     private boolean[] used;
 
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        int len = nums.length;
+        if (len == 0) {
+            return res;
+        }
+        // 修改 1：首先排序，之后才有可能发现重复分支
+        Arrays.sort(nums);
+        used = new boolean[len];
+        findPermuteUnique(nums, 0, new Stack<>());
+        return res;
+    }
+
     /**
      * 官方解题， 注意回溯算法的剪枝处理
      * @param nums
@@ -39,7 +51,7 @@ public class PermuteUnique {
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if (!used[i]) {                                             // 剪枝 1， 位置不能够重复使用
+            if (!used[i]) {      // 剪枝 1， 位置不能够重复使用
                 // 修改 2：因为排序以后重复的数一定不会出现在开始，故 i > 0
                 // 和之前的数相等，并且之前的数还未使用过，只有出现这种情况，才会出现相同分支
                 // 这种情况跳过即可
@@ -55,17 +67,6 @@ public class PermuteUnique {
         }
     }
 
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        int len = nums.length;
-        if (len == 0) {
-            return res;
-        }
-        // 修改 1：首先排序，之后才有可能发现重复分支
-        Arrays.sort(nums);
-        used = new boolean[len];
-        findPermuteUnique(nums, 0, new Stack<>());
-        return res;
-    }
 
     public static void main(String[] args) {
         int[] nums = {1, 1, 2};
