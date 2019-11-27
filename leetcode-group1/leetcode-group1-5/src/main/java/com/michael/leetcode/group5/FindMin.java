9284a88f0e5367c2fee197c9051ad493f181df6a
Michael <13432849094@163.com>
2019-11-27 14:48:08 +0800

    旋转数组查找最小值

--- leetcode-group1/leetcode-group1-5/src/main/java/com/michael/leetcode/group5/FindMin.java
+++ leetcode-group1/leetcode-group1-5/src/main/java/com/michael/leetcode/group5/FindMin.java
@@ -4,19 +4,22 @@ public class FindMin {
 
     /**
      * 滑动指针
+     *
      * @param nums
      * @return
      */
     public int findMin(int[] nums) {
 
-        if (nums.length == 0) return 0;
-        if (nums.length == 1) return nums[0];
+        int length = nums.length;
+
+        if (length == 0) return 0;
+        if (length == 1) return nums[0];
 
         int start = 0, end = 1;
 
         int result = nums[0];
 
-        while(end < nums.length) {
+        while (end < length) {
             if (nums[start] > nums[end]) {
                 return nums[end];
             } else {
@@ -59,20 +62,48 @@ public class FindMin {
         return -1;
     }
 
+    /**
+     * 存在重复的情况
+     *
+     * 这个解法可以画图表示，通过中间点的大小与右边的大小进行比较来确定往哪一遍靠拢
+     *
+     * @param nums
+     * @return
+     */
+    public int findMin3(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[right]) left = mid + 1;
+            else if (nums[mid] < nums[right]) right = mid;
+            else right = right - 1;
+        }
+        return nums[left];
+
+//        作者：jyd
+//        链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii/solution/154-find-minimum-in-rotated-sorted-array-ii-by-jyd/
+//        来源：力扣（LeetCode）
+//        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    }
+
 //    作者：LeetCode
 //    链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/solution/xun-zhao-xuan-zhuan-pai-lie-shu-zu-zhong-de-zui-xi/
 //    来源：力扣（LeetCode）
 //    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
         FindMin findMin = new FindMin();
 
+//        int[] data = new int[]{ 0, 1, 2, 4, 5, 6, 7};
 //        int[] data = new int[]{4, 5, 6, 7, 0, 1, 2};
 
-        int[] data = new int[]{};
+//        int[] data = new int[]{3,3,3,2};
+//        int[] data = new int[]{2,2,2,3};
+//        int[] data = new int[]{2,2,2};
+        int[] data = new int[]{0, 1, 1, 3, 1};
 
-        System.out.println(findMin.findMin(data));
+        System.out.println(findMin.findMin2(data));
 
     }
 
