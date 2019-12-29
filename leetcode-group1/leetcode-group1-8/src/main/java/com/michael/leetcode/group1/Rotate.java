6b28e519c9ac0feb7abd271bc26adce5068f7492
Michael <13432849094@163.com>
2019-12-29 20:02:34 +0800

    添加数组反转

--- leetcode-group1/leetcode-group1-8/src/main/java/com/michael/leetcode/group1/Rotate.java
+++ leetcode-group1/leetcode-group1-8/src/main/java/com/michael/leetcode/group1/Rotate.java
@@ -29,4 +29,133 @@ package com.michael.leetcode.group1;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Rotate {
+
+
+    /**
+     * 时间复杂度 O(n2)
+     * @param nums
+     * @param k
+     */
+    public void rotate(int[] nums, int k) {
+
+        for (int i = 0; i < k; i++) {
+            int temp = nums[nums.length - 1];
+            int j = nums.length-1;
+
+            while (j > 0) {
+                nums[j] = nums[j - 1];
+                j--;
+            }
+            nums[0] = temp;
+        }
+    }
+
+
+    /**
+     * 官方
+     * 环替换
+     * @param nums
+     * @param k
+     */
+    public void rotate4(int[] nums, int k) {
+        k = k % nums.length;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+    }
+//    作者：LeetCode
+//    链接：https://leetcode-cn.com/problems/rotate-array/solution/xuan-zhuan-shu-zu-by-leetcode/
+//    来源：力扣（LeetCode）
+//    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+    /**
+     * 官方：
+     *
+     * 先整体反转，在找到断点，反转前段，后反转后段
+     * @param nums
+     * @param k
+     */
+    public void rotate5(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    // 这里的反转的小技能
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;           // 前后同时向中间靠拢
+            end--;
+        }
+    }
+
+//    作者：LeetCode
+//    链接：https://leetcode-cn.com/problems/rotate-array/solution/xuan-zhuan-shu-zu-by-leetcode/
+//    来源：力扣（LeetCode）
+//    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+    /**
+     * 折半反转，以中间为界限，nums[i] <-> nums[nums.len - 1 - i]
+     * 必须在原数组上进行操作
+     * @param nums
+     */
+    public void rotate3(int[] nums) {
+
+        int mid = nums.length / 2;
+
+        for (int i = 0; i < mid; i++) {
+            int temp = nums[i];
+            int index = nums.length - 1 - i;
+            nums[i] = nums[index];
+            nums[index] = temp;
+        }
+    }
+
+    /**
+     * 反转数组
+     * 空间复杂度不符合要求
+     */
+    public int[] rotate2(int[] nums) {
+
+        int[] res = new int[nums.length];
+
+        for (int i = nums.length-1; i >= 0; i--) {
+            int index = res.length - 1 - i;
+            res[index] = nums[i];
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        Rotate rotate = new Rotate();
+
+        int[] data = new int[]{1, 2, 3, 4, 5, 6};
+
+//        int[] result = rotate.rotate2(data);
+//        rotate.rotate3(data);
+
+        rotate.rotate(data, 3);
+
+        for (int i : data) {
+            System.out.println(i);
+        }
+
+    }
+
 }
