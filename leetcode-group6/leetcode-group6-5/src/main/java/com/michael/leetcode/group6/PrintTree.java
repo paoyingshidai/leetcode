431bd0fce2b7b7a95f1629fd0762c15177e61372
Michael <13432849094@163.com>
2021-01-08 18:08:46 +0800

    二叉树剪枝

---
+++ leetcode-group6/leetcode-group6-5/src/main/java/com/michael/leetcode/group6/PrintTree.java
@@ -0,0 +1,26 @@
+package com.michael.leetcode.group6;
+
+import java.util.List;
+
+/**
+ * @author Michael
+ */
+public class PrintTree {
+
+    public List<List<String>> printTree(TreeNode root) {
+
+        return null;
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
