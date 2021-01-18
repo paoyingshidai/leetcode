0d1f1503a457f7396e669cc87edf3dac25ccf9d0
PC-20200926MBQT\Administrator <13432849094@163.com>
2021-01-18 23:18:56 +0800

    数组

---
+++ offer/offer-0/src/main/java/com/michael/leetcode/offer/MaxSubArray.java
@@ -0,0 +1,44 @@
+package com.michael.leetcode.offer;
+
+/**
+ * 剑指 Offer 42. 连续子数组的最大和
+ * 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+ *
+ * 要求时间复杂度为O(n)。
+ *
+ *
+ *
+ * 示例1:
+ *
+ * 输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ *
+ *
+ * 提示：
+ *
+ * 1 <= arr.length <= 10^5
+ * -100 <= arr[i] <= 100
+ */
+public class MaxSubArray {
+
+
+    /**
+     * 动态规划
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            nums[i] += Math.max(nums[i - 1], 0);
+            res = Math.max(res, nums[i]);
+        }
+        return res;
+    }
+
+//    作者：jyd
+//    链接：https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/solution/mian-shi-ti-42-lian-xu-zi-shu-zu-de-zui-da-he-do-2/
+//    来源：力扣（LeetCode）
+//    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+}
---
+++ offer/offer-0/src/main/java/com/michael/leetcode/offer/MissingNumber.java
@@ -0,0 +1,49 @@
+package com.michael.leetcode.offer;
+
+/**
+ * 剑指 Offer 53 - II. 0～n-1中缺失的数字
+ * 一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: [0,1,3]
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: [0,1,2,3,4,5,6,7,9]
+ * 输出: 8
+ *
+ *
+ * 限制：
+ *
+ * 1 <= 数组长度 <= 10000
+ */
+public class MissingNumber {
+
+    /**
+     * 二分法
+     * @param nums
+     * @return
+     */
+    public int missingNumber(int[] nums) {
+
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == mid) {
+                left = mid + 1;
+            } else if (nums[mid] > mid) {
+                right = mid - 1;
+            }
+        }
+        return left;
+
+//        作者：lan-zhe-xian
+//        链接：https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof/solution/xiang-jie-er-fen-cha-zhao-de-liang-chong-kai-bi-qi/
+//        来源：力扣（LeetCode）
+//        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    }
+
+}
